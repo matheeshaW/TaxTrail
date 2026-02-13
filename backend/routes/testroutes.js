3bd777998b415ede6b9888b4bc6d19b3ee25352d
matheeshaW <matheeshaweerakoon22@gmail.com>
2026-02-13 23:33:52 +0530

    controllers implemented for test , working crud api's

--- backend/routes/testroutes.js
+++ backend/routes/testroutes.js
@@ -1,41 +1,30 @@
 const express = require('express')
-const Test = require('../models/testModel')
-const mongoose = require('mongoose')
+const { 
+    createTest,
+    getTests,
+    getTest,
+    deleteTest,
+    updateTest
+ } = require('../controllers/testController')
+
 
 const router = express.Router()
 
 //GET all test files
-router.get('/', (req, res) => {
-    res.json({mssg: 'get all test files'})
-})
+router.get('/', getTests)
 
 //GET a single test file
 
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'get a single test file'})
-})
+router.get('/:id', getTest)
 
 //POST a new test file
-router.post('/', async (req, res) => {
-    const {title, content} = req.body
-    try{
-        const test = await Test.create({title, content})
-        res.status(200).json(test)
-    }
-    catch(error){
-        res.status(400).json({error: error.message})
-    }
-    
-})
+router.post('/', createTest)
 
 //DELETE a test file
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'delete a test file'})
-})
+router.delete('/:id', deleteTest
+)
 
 //UPDATE a test file
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'update a test file'})
-})
+router.patch('/:id', updateTest)
 
 module.exports = router
