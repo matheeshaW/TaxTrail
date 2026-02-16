a13d8b0f934c3a9863bfd7470d95e77cc94fabfa
kanushka <kanushkakahakotuwa2001@gmail.com>
2026-02-16 15:51:59 +0530

    add SocialProgram model with schema validation and relationships

---
+++ backend/models/SocialProgram.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const socialProgramSchema = new mongoose.Schema({
+    programName : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    sector : {
+        type : String,
+        enum : ['Welfare','Education','Health','Housing','Food Assistance'],
+        required : true
+    },
+    targetGroup : {
+        type : String,
+        enum : ['Low Income', 'Middle Income', 'Rural', 'Urban Poor', 'Disabled'],
+        required : true
+    },
+    beneficiariesCount : {
+        type : Number,
+        required : true,
+        min : 0
+    },
+    budgetUsed : {
+        type : Number,
+        required : true,
+        min : 0
+    },
+    year : {
+        type : Number,
+        required : true,
+        validate : {
+            validator : function (value) {
+            return value <= new Date().getFullYear();
+        },
+        message : 'Year cannot be in the future'
+        }
+    },
+    region : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Region',
+        required : true
+    },
+    createdBy : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required : true
+    }
+
+} , {timestamps : true})
+
+module.exports = mongoose.model('SocialProgram', socialProgramSchema)
