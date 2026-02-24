cb903c19577617bbe6c01986c3a6e52d4679a3aa
Nirmana <nirmanaherath@gmail.com>
2026-02-24 11:05:16 +0530

    Add other necessory routes for budgetAllocationRoutes file

--- backend/routes/budgetAllocationRoutes.js
+++ backend/routes/budgetAllocationRoutes.js
@@ -3,9 +3,21 @@ const router = express.Router();
 
 const {
   createAllocation,
+  getAllAllocations,
+  getSingleAllocation,
+  updateAllocation,
+  deleteAllocation,
+  getSummaryBySector,
 } = require("../controllers/budgetAllocationController");
 
-// POST /api/v1/budget-allocations
-router.post("/", createAllocation);
+router.get("/summary/by-sector", getSummaryBySector);
+
+router.route("/").post(createAllocation).get(getAllAllocations);
+
+router
+  .route("/:id")
+  .get(getSingleAllocation)
+  .put(updateAllocation)
+  .delete(deleteAllocation);
 
 module.exports = router;
